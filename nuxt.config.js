df271c2ce4ad3a71f494707ca377e8bfc789a384
philip gazagnes <p.gazagnes@vacanceselect.com>
2020-04-08 12:29:40 +0200

    init

---
+++ nuxt.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1,maximum-scale=1.0, user-scalable=no',
+      },
+    ],
+    link: [
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Lato:400|Playball&display=swap',
+      },
+    ],
+  },
+  modules: ['@nuxtjs/style-resources', '@nuxtjs/svg'],
+  plugins: ['~/plugins/lazyload.client.js', '~/plugins/in-view.client.js'],
+  css: ['~/assets/scss/main.scss'],
+  styleResources: {
+    scss: ['~/assets/scss/vars.scss'],
+  },
+};
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="content">
+    <Logo />
+    <h1>Veuve Michel</h1>
+    <p>Site en construction, mise en ligne prévue pour juin 2020</p>
+  </div>
+</template>
+
+<script>
+import Logo from '~/assets/svg/logo.svg?inline';
+
+export default {
+  components: {
+    Logo,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.content {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  width: 300px;
+  svg {
+    width: 200px;
+    height: 200px;
+    fill: $colorSoil;
+  }
+  p {
+    line-height: 1.5em;
+  }
+}
+</style>
